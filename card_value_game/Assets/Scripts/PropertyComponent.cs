17d24f9ea295725fc2b61943c68ae6d610ab00d1
williambl <william@williambl.com>
2018-01-12 21:02:46 +0000

    Added Property Component

---
+++ card_value_game/Assets/Scripts/PropertyComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PropertyComponent : MonoBehaviour {
+
+    public string propertyName;
+    public int propertyValue;
+
+    Text nameText;
+    Text valueText;
+    Image valueSlider;
+    
+    void Start () {
+        nameText = transform.Find("Name").GetComponent<Text>();
+        valueText = transform.Find("Slider/Text").GetComponent<Text>();
+        valueSlider = transform.Find("Slider").GetComponent<Image>();
+
+        nameText.text = propertyName;
+    }
+
+    void Update () {
+        valueText.text = propertyValue.ToString();
+        valueSlider.fillAmount = propertyValue / 100;
+    }
+	
+}
