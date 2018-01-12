d84c2e10fdbb45cd0b3f7859f4b2d71911c3120d
williambl <william@williambl.com>
2018-01-12 20:07:27 +0000

    Added a card class

---
+++ card_value_game/Assets/Scripts/Card.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class Card {
+
+    string name;
+    Dictionary<string, int> properties;
+
+    public Card (Dictionary<string, int> propertiesIn, string nameIn) {
+        name = nameIn;
+        properties = propertiesIn;
+    }
+
+    public void addProperty (string propName, int propValue) {
+        properties.Add(propName, propValue);
+    }
+}
