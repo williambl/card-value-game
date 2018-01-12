6580cea11ad50b3e1074d0e17a4795f7a9b41a02
williambl <william@williambl.com>
2018-01-12 21:21:05 +0000

    Added Card Creator

---
+++ card_value_game/Assets/Scripts/CardCreator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardCreator : MonoBehaviour {
+
+    public Card testCard;
+    public GameObject cardObj;
+
+    void Start () {
+        Dictionary<string, int> testCardDict = new Dictionary<string, int>();
+        testCardDict.Add("test property", 50);
+        testCard = new Card(testCardDict, "testCard");
+
+        GameObject newCardObj = Instantiate(cardObj);
+        newCardObj.GetComponent<CardComponent>().card = testCard;
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+}
