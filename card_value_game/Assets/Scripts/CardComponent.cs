4336ce3441105360a49192534f6e90bce70a9935
williambl <william@williambl.com>
2018-01-12 22:18:05 +0000

    Card name now shows on card

--- card_value_game/Assets/Scripts/CardComponent.cs
+++ card_value_game/Assets/Scripts/CardComponent.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardComponent : MonoBehaviour {
 
@@ -8,6 +9,8 @@ public class CardComponent : MonoBehaviour {
     public GameObject propertyObj;
 
     void Start () {
+        transform.Find("Name").GetComponent<Text>().text = card.name;
+
         int i = 0;
         foreach (var property in card.properties) {
             GameObject newProperty = Instantiate(propertyObj, transform);
